7ff96d049ad3fab97ed1c686daa55910c80ff280
Devin Fallak <dmfallak@gmail.com>
2026-04-10 11:27:11 -0400

    feat: add database client

---
+++ tests/lib/db.test.ts
@@ -0,0 +1,17 @@
+import { expect, test, vi } from 'vitest';
+
+vi.mock('pg', () => {
+  const Pool = vi.fn(function() {
+    this.query = vi.fn().mockResolvedValue({ rows: [{ val: 1 }] });
+  });
+  return {
+    default: { Pool },
+  };
+});
+
+import { db } from '../../src/lib/db.js';
+
+test('db should be able to query', async () => {
+  const result = await db.query('SELECT 1 as val');
+  expect(result.rows[0].val).toBe(1);
+});
